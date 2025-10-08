aaaa99dba6639c6b47f8cc3017e31bcd2bfae544
Leafeon4T <beanmicha@gmail.com>
2025-10-08 13:08:30 -0600

    Player infrastructure added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 
@@ -9,12 +10,18 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	dt = 0
+	clock = pygame.time.Clock()
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(("black"))
+		player.draw(screen)
 		pygame.display.flip()
+	dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
